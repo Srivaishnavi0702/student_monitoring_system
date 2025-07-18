e6c33c9e8193de4c4657c4795610c18b8bbdb5e0
Srivaishnavi0702 <anbusrivaishnavi@gmail.com>
2025-07-19 00:08:52 +0530

    Update app_pathway.dart

--- student_flutter/lib/navigation/app_pathway.dart
+++ student_flutter/lib/navigation/app_pathway.dart
@@ -1,10 +1,12 @@
 import '../screens/login_screen.dart';
+import '../screens/splash_screen.dart';
 // Later you will import other screens here like:
 // '../screens/signup_screen.dart';
 // '../screens/student_dashboard.dart';
 
 class AppPathway {
   static final routes = {
+    '/splash': (context) => const SplashScreen(),
     '/login': (context) => LoginScreen(),
     // Add more routes here as you create screens
     // '/signup': (context) => SignupScreen(),
